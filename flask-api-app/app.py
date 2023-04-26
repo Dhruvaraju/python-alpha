045f33e9042d08b96324a4db0b29f58c51e0d149
Dhruva Raju <49dhruva@gmail.com>
2023-04-26 22:19:35 +0530

    feat(flask): base api post

--- flask-api-app/app.py
+++ flask-api-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,3 +17,10 @@ stores = [
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
+
+@app.post("/store")
+def add_new_store():
+    request_date = request.get_json()
+    new_store = {"name": request_date["name"]}
+    stores.append(new_store)
+    return new_store, 201
--- flask-rest-api.md
+++ flask-rest-api.md
@@ -5,4 +5,10 @@
 - create a folder and switch to it
 - Use `python -m venv .venv`
 - Use `ctrl+shift+p` to choose the interpreter to the virtual environment that is created
-P
+
+
+## Initial endpoint
+- Create a method and return any value.
+- `@app.get("/store")` add this to bind a uri to a method
+- The return of the method will be sent as a response to this, endpoint.
+
